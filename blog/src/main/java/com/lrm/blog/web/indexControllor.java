b0bd95e0623e18748d216e679fbddab181cd6871
heinegyo <heinegyo@gmail.com>
2020-10-16 15:04:35 +0800

    add LogAspect

--- blog/src/main/java/com/lrm/blog/web/indexControllor.java
+++ blog/src/main/java/com/lrm/blog/web/indexControllor.java
@@ -3,19 +3,20 @@ package com.lrm.blog.web;
 import com.lrm.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexControllor {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
 
-        String blog  = null;
-
-        if(blog == null){
-            throw new NotFoundException("此篇文章已經不存在了");
-        }
-
+//        String blog  = null;
+//
+//        if(blog == null){
+//            throw new NotFoundException("此篇文章已經不存在了");
+//        }
+        System.out.println("--index---");
         return "index";
     }
 }
