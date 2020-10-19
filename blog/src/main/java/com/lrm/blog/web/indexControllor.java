37d33eaaec10fee6355f96f6c36d51b4de92b55e
heinegyo <heinegyo@gmail.com>
2020-10-19 10:34:39 +0800

    add fragment

--- blog/src/main/java/com/lrm/blog/web/indexControllor.java
+++ blog/src/main/java/com/lrm/blog/web/indexControllor.java
@@ -8,15 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class indexControllor {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name) {
+    @GetMapping("/")
+    public String index() {
 
 //        String blog  = null;
 //
 //        if(blog == null){
 //            throw new NotFoundException("此篇文章已經不存在了");
 //        }
-        System.out.println("--index---");
+//        System.out.println("--index---");
         return "index";
     }
 }
