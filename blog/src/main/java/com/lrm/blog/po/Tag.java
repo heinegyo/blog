e4d5828bd1c899bd795eb7ea1cb3ec04b3b85f32
heinegyo <heinegyo@gmail.com>
2020-10-19 14:02:39 +0800

    Type

---
+++ blog/src/main/java/com/lrm/blog/po/Tag.java
@@ -0,0 +1,5 @@
+package com.lrm.blog.po;
+
+public class Tag {
+    
+}
---
+++ blog/src/main/java/com/lrm/blog/po/Type.java
@@ -0,0 +1,36 @@
+package com.lrm.blog.po;
+
+
+
+public class Type {
+    private Long id;
+    private String name;
+
+    public Type(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
